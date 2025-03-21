a5dd2889b88e4bb8880c43ca6923abccd31796d7
TheAnalyticsCareer <careeranalytics499@gmail.com>
2025-03-21 16:53:34 +0530

    commit message

--- src/home/ViewPurchase.jsx
+++ src/home/ViewPurchase.jsx
@@ -451,7 +451,7 @@ const ViewPurchase = () => {
   return (
     <div className="container mt-4">
       <div className="text-center text-light bg-dark p-4 rounded shadow-lg w-50 mx-auto my-5">
-        <h3 className="mb-3">Purchase History</h3>
+        <h3 className="mb-3">Purchaseeeeeee History</h3>
 
         <div className="d-flex justify-content-center gap-3 mb-3">
           <input
