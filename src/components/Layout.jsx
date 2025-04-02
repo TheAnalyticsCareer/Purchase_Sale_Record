b76c418ed74a5d0534a9957262b11c8ff48cae82
TheAnalyticsCareer <careeranalytics499@gmail.com>
2025-04-02 20:53:15 +0530

    update code

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,13 +4,11 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Layout() {
-
-
   useEffect(() => {
     let navigate = useNavigate();
     let department = localStorage.getItem("department");
     let token = localStorage.getItem("token");
-
+    alert("navigating to department");
     if (token) {
       navigate(`/${department}`);
     }
