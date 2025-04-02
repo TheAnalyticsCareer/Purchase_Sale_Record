6f590762ab57bfc5942622d69e562db6a320616f
TheAnalyticsCareer <careeranalytics499@gmail.com>
2025-04-02 20:19:35 +0530

    update code

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "./header/Header"; 
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Layout() {
+  
+  let navigate=useNavigate();
+  let department = localStorage.getItem("department");
+
+useEffect(()=>{
+  navigate(`/${department}`)
+},[])
+
   return (
     <>
       <Header />
